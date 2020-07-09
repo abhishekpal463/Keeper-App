2285425efae8b1c80e3059197bd19d7ecc256301
abhishekpal463 <61150689+abhishekpal463@users.noreply.github.com>
2020-07-09 17:32:50 +0530

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
