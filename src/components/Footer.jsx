781f33297b4f6482cd6c2f16e9927488c5dd1fc1
abhishekpal463 <61150689+abhishekpal463@users.noreply.github.com>
2020-07-11 12:46:35 +0530

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 
 function Footer() {
-  const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>Copyright ⓒ Abhishek</p>
     </footer>
   );
 }
